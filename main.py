0e868cb8bd50fb6e9f8ef24b7039cbecb43d2116
Akshay-Jhijhautia <akshay.jhijhautia@gmail.com>
2025-09-24 09:21:26 +0530

    pdf merger complete

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-from PyPDF2 import PdfWriter
+from PyPDF2 import PdfMerger
 
-merger = PdfWriter()
+merger = PdfMerger()
+
+pdfs = ["Nextjs.pdf", "Styling.pdf"]
+
+for pdf in pdfs:
+    merger.append(pdf)
+
+with open("merged-pdf.pdf", "wb") as f:
+    merger.write(f)
+
+merger.close()
